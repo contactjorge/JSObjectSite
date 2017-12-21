2191b38fe33f70b173733311ed09eff141e54539
contactjorge <contactjorge@thejorgelopez.com>
2017-12-20 18:35:30 -0600

    Updated data feed: Added more data layers. Corrections to People cards. Addition of Neil DeGrasse Tyson Stub functions for "Site Builder" functions.
    Next:
    Separate functions into two files: "Site Data" and "Site Builder".
    Continued build out of data object.

--- site_data.js
+++ site_data.js
@@ -1,12 +1,22 @@
 let siteData = {
-	"Site" : "Internet Video Database", //used to change the site name
+	"Title": "Internet Video Database", //used to change the site name
 	"Owner" : "Walt Disney Studios", //used to change the ?
-	"navLinks" : [
-		{ "About" : "http://thejorgelopez.com"},
-		{ "Portfolio" : "http://thejorgelopez.com"},
-		{ "Blog" : "http://thejorgelopez.com"},
-		{ "Resume" : "http://thejorgelopez.com"},
-	],
+	"Header": [{
+		"Logo": "logo.png", //Logo for the NavBar
+		"NavBar": {
+			"About": "http://thejorgelopez.com",
+			"Portfolio": "http://thejorgelopez.com",
+			"Blog": "http://thejorgelopez.com",
+			"Resume": "http://thejorgelopez.com"
+		},// NavBar Elements with URL
+		"Header_Images": {
+			"image00": "image01.png",
+			"image01": "image02.png",
+			"image02": "image03.png",
+			"image03": "image04.png",
+			"image04": "image05.png"
+		}// Dummy NavBar Images
+	}],
 	"Main" : [{ //Notice this is an array with 1 item that has items in it. This is intentional to represent "dirty data feeds"
 		"Sections": [
 			{"Section0": ""},
@@ -15,12 +25,12 @@ let siteData = {
 			{"Section3": ""},
 			{"Section4": ""},
 			{"Section5": ""}
-		],
+		],//Challenge: Check for null section content
 		"Articles": [ //Notice this is an array with 1 item that has items in it. This is intentional to represent "dirty data feeds"
 			{"Article0": ""},
 			{"Article1": ""},
 			{"Article2": ""},
-			{"Article3": ""}
+			{"Article3": ""}//Challenge: Check for null article content
 		],
 		"People": {  //People data is a standard object with key : field pairs. Why would the above examples have [ ] around them? (part of the challenge)
 				"fName": "Jorge",
@@ -42,48 +52,73 @@ let siteData = {
 						"Title": "E.T.",
 						"Year": "1982"
 					}
-				]},
-				"fName": "John",
-				"lName": "Smith",
-				"Movies": [
-					{
-						"Title": "It's a Wonderful Life",
-						"Year": "1946"
-					},
-					{
-						"Title": "Justice League",
-						"Year": "2017"
-					},
-					{
-						"Title": "Muppets Take Manhattan",
-						"Year": "1984"
-					},
-					{
-						"Title": "Grease",
-						"Year": "1978"
-					}
-				]},
-			{
-				"fName": "Sarah-Jane",
-				"lName": "Smith",
-				"Movies": [
-					{
-						"Title": "Batman",
-						"Year": "1980"
-					},
-					{
-						"Title": "Star Wars: Force Awakens",
-						"Year": "2015"
-					},
-					{
-						"Title": "Star Trek",
-						"Year": "1979"
-					},
-					{
-						"Title": "Gone With the Wind",
-						"Year": "1939"
-					}
-				]}],
+				]
+		},//End Person card 1. Challenge: Sort movies by year
+		{
+			"fName": "John",
+			"lName": "Smith",
+			"Movies": [
+				{
+					"Title": "It's a Wonderful Life",
+					"Year": "1946"
+				},
+				{
+					"Title": "Justice League",
+					"Year": "2017"
+				},
+				{
+					"Title": "Muppets Take Manhattan",
+					"Year": "1984"
+				},
+				{
+					"Title": "Grease",
+					"Year": "1978"
+				}]
+		},//End Person card 2. Challenge: Sort movies by year
+		{
+			"fName": "Sarah-Jane",
+			"lName": "Smith",
+			"Movies": [
+				{
+					"Title": "Batman",
+					"Year": "1980"
+				},
+				{
+					"Title": "Star Wars: Force Awakens",
+					"Year": "2015"
+				},
+				{
+					"Title": "Star Trek",
+					"Year": "1979"
+				},
+				{
+					"Title": "Gone With the Wind",
+					"Year": "1939"
+				}]
+		},//End Person card 3. Challenge: Sort movies by year
+		{
+			"fName": "Neil DeGrasse",
+			"lName": "Tyson",
+			"Movies": [
+				{
+					"Title": "Batman vs Superman",
+					"Year": "2015"
+				},
+				{
+					"Title": "Contact",
+					"Year": "1997"
+				},
+				{
+					"Title": "Paper Moon",
+					"Year": "1973"
+				},
+				{
+					"Title": "Young Frankenstein",
+					"Year": "1974"
+				}
+			]
+		}//End Person card 4. Challenge: Sort movies by year
+	],//End Person Content Challenge: Sort by Last Name
 	"Footer" : { //build the footer social media nav element with links
 		"socialMedia" : {
 			"blog"     : "http://sithguidetogalaxy.com",
@@ -91,13 +126,21 @@ let siteData = {
 			 "youtube"  : "https://www.youtube.com/user/BinauralBrainwave",
 			 "instagram"  : "https://www.instagram.com/downtownevanston/",
 			 "twitter"  : "https://twitter.com/repkellycassidy"
-		}
+		}//Social Media Links
 	}
 };//End Site Data Object
 
 function getSiteData() {
+	let footerData = siteData.Footer;
+	let headerData = siteData.Header
+}//end getSiteData Data for site builder
 
-}//end getSiteData Data and site builder
+
+function setSiteContent() {
+	getNavi(); //returns the top navigation bar.
+	getMainContent(); //returns the top navigation bar.
+	getFooter(); //returns the top navigation bar.
+}//end getSiteContent
 
 function getNavi() {
 	let updateNavBar = document.getElementById('top-navi');
@@ -113,11 +156,14 @@ function getNavi() {
 }//end getNavi() navbar builder
 
 function getSections() {
-
+	let updateSections = document.getElementById('main-semantic');
+	let sectionContent = '';
 }//end getSections Data and section builder
 
 
 function getArticles() {
+	let updateArticles = document.getElementById('main-semantic');
+	let articleContent = '';
 
 }//end getArticles Data and article builder
 
@@ -136,5 +182,8 @@ function getFooter() {
 	return updateFooter.innerHTML = socialMediaLinks;
 }//end geFooteri() navbar builder
 
-
-
+function getMainContent() {
+	getArticles();
+	getSections();
+	getPeopleCards();
+}
